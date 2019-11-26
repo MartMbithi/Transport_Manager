5ae7326649bbad947b26e9871a78561aab61e2cc
martin <martdevelopers254@gmail.com>
2019-11-26 16:04:30 +0300

    Implemented all functionalities

---
+++ admin/vendor/inc/footer.php
@@ -0,0 +1,9 @@
+<footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright © <?php echo date('Y');?> Transport Management System |  Crafted with <i class ="fa fa-heart"></i> by 
+                <a href ="https://martdev.info" target="_blank">Esther Ngundo</a>
+           </span>
+          </div>
+        </div>
+</footer>
---
+++ vendor/inc/footer.php
@@ -0,0 +1,8 @@
+<footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; <?php echo date('Y');?> Transport Management System | Crafted With <i class ="fa fa-heartbeat"></i> By 
+        <a href="'https://martmbithi.github.io">Esther Ngundo</a>   
+       </p>
+    </div>
+    <!-- /.container -->
+  </footer>
